22dc0b66a3ff8172c369eb21e93dea76d98d7d4b
trr <trrbader@gmail.com>
2024-07-02 09:48:19 +0300

    add filtering and update and check functionality

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,35 +1,106 @@
 import { UilCheck } from "@iconscout/react-unicons";
 import { UilTrashAlt } from "@iconscout/react-unicons";
 import { UilEdit } from "@iconscout/react-unicons";
-import { useContext } from "react";
+import { UilCheckCircle } from "@iconscout/react-unicons";
+import { useContext, useEffect, useRef, useState } from "react";
 import { TodosContext } from "../contexts/todosContext";
 
-const Todo = ({ id, title }) => {
+const Todo = ({ todo }) => {
   const { todos, setTodos } = useContext(TodosContext);
+  const [isEditable, setIsEditable] = useState(false);
+  const [isCheck, setIsCheck] = useState(false);
+  const inputRef = useRef();
+  const checkRef = useRef();
+
+  useEffect(() => {
+    if (todo.isDone) {
+      checkRef.current.classList += " clicked";
+    } else {
+      checkRef.current.classList = "label";
+    }
+  }, [todo]);
 
   const handleDelete = () => {
     const updatedTodos = todos.filter((t) => {
-      return t.id != id;
+      return t.id != todo.id;
     });
+    setTodos(updatedTodos);
+    setIsCheck(false);
+  };
+
+  const handleUpdate = () => {
+    const newTitle = inputRef.current.value;
+    const updatedTodos = todos.map((t) => {
+      if (t.id === todo.id) {
+        return { ...t, title: newTitle };
+      } else {
+        return t;
+      }
+    });
+    setIsEditable(false);
     setTodos(updatedTodos);
   };
 
+  const handleCheck = () => {
+    if (!isCheck) {
+      checkRef.current.classList += " clicked";
+      const updatedTodos = todos.map((t) => {
+        if (t.id === todo.id) {
+          // checkRef.current.classList += " clicked";
+          return { ...t, isDone: !todo.isDone };
+        } else {
+          return t;
+        }
+      });
+      setIsCheck(true);
+      setTodos(updatedTodos);
+    } else {
+      checkRef.current.classList = "label";
+      setIsCheck(false);
+    }
+
+    console.log(todos);
+  };
+
   return (
-    <li key={id} className="todo__item">
-      <div className="label">
+    <li key={todo.id} className="todo__item">
+      <div className="label" ref={checkRef}>
         <span className="check__box">
-          <UilCheck className="check_icon" size="32" color="#a6a6a6" />
+          <UilCheck
+            className="check_icon"
+            size="32"
+            color="#a6a6a6"
+            onClick={handleCheck}
+          />
         </span>
-        <span className="item__title">{title}</span>
+        <span className="item__title">
+          {isEditable ? <input ref={inputRef} /> : todo.title}
+        </span>
       </div>
       <div className="icons__section">
-        <UilTrashAlt
-          className="trash_icon"
-          size="24"
-          color="#a6a6a6"
-          onClick={handleDelete}
-        />
-        <UilEdit className="edit_icon" size="24" color="#a6a6a6" />
+        {isEditable ? (
+          <UilCheckCircle
+            className="check_update_icon"
+            size="24"
+            color="#4ca1f7"
+            onClick={handleUpdate}
+          />
+        ) : (
+          <>
+            <UilTrashAlt
+              className="trash_icon"
+              size="24"
+              color="#a6a6a6"
+              onClick={handleDelete}
+            />
+            <UilEdit
+              className="edit_icon"
+              size="24"
+              color="#a6a6a6"
+              onClick={() => setIsEditable(true)}
+            />
+          </>
+        )}
       </div>
     </li>
   );
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -24,12 +24,37 @@ import { TodosContext } from "../contexts/todosContext";
 const TodoList = () => {
   // const [tasks, setTasks] = useState(initialTodos);
   const { todos, setTodos } = useContext(TodosContext);
+  const [value, setValue] = useState("all");
   const inputRef = useRef("");
+  let todosWillRender = todos;
+
+  const doneTodos = todos.filter((t) => {
+    return t.isDone;
+  });
+
+  const notDoneTodos = todos.filter((t) => {
+    return !t.isDone;
+  });
 
   const handelInput = () => {
     const currentValue = inputRef.current.value;
     setTodos([...todos, { id: "4", title: [currentValue], isDone: false }]);
   };
+
+  const handleFilterChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  if (value === "completed") {
+    todosWillRender = doneTodos;
+  } else if (value === "unCompleted") {
+    todosWillRender = notDoneTodos;
+  } else {
+    todosWillRender = todos;
+  }
+
+  console.log(todos);
+  console.log(todosWillRender);
 
   return (
     <div className="container">
@@ -44,15 +69,15 @@ const TodoList = () => {
         />
       </div>
       <div class="newItem__select">
-        <select class="filter">
+        <select class="filter" value={value} onChange={handleFilterChange}>
           <option value="all">الكل</option>
           <option value="completed">مكتمل</option>
           <option value="unCompleted">غير مكتمل</option>
         </select>
       </div>
       <ul className="todo__list">
-        {todos.map((todo) => (
-          <Todo id={todo.id} title={todo.title} />
+        {todosWillRender.map((todo) => (
+          <Todo todo={todo} />
         ))}
       </ul>
     </div>
